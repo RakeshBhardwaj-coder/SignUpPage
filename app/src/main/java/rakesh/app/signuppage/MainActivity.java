1f26e14c672e1fe0602fb89504daa3ac7287ef14
rakesh bhardwaj <rb610883@gmail.com>
2023-10-19 23:13:01 +0530

    sign up page designed.

---
+++ app/src/main/java/rakesh/app/signuppage/MainActivity.java
@@ -0,0 +1,39 @@
+package rakesh.app.signuppage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView googleImage,facebookImage,twitterImage;
+    Button signUpPageBtn;
+    TextView loginPageBtn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        googleImage = findViewById(R.id.googleImg);
+        facebookImage = findViewById(R.id.facebookImg);
+        twitterImage = findViewById(R.id.twitterImg);
+
+        signUpPageBtn = findViewById(R.id.SignUpBtn);
+        loginPageBtn = findViewById(R.id.LoginPageBtn);
+
+
+        signUpPageBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), SignUpPage.class));
+
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/rakesh/app/signuppage/SignUpPage.java
@@ -0,0 +1,27 @@
+package rakesh.app.signuppage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class SignUpPage extends AppCompatActivity {
+
+    EditText firstName,lastName,email,password,confirmPassword;
+    Button signUpBtn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up_page);
+
+        firstName = findViewById(R.id.firstNameText);
+        lastName = findViewById(R.id.lastNameText);
+        email = findViewById(R.id.emailText);
+        password = findViewById(R.id.passwordText);
+        confirmPassword = findViewById(R.id.confirmPasswordText);
+
+
+
+    }
+}
